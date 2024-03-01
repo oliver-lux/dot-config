fca8bdef37868b761d340dd35ef7c7d62132ed80
Oliver Lux <oliver.lux@outlook.de>
2024-03-01 13:38:36 +0100

    Change init of zoxide

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,18 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    
+    # disable fish greeting
+    set -U fish_greeting
+
+    # fix sorting of files
+    export LC_COLLATE="C"
+
+    # change the color of the directories
+    set LS_COLORS ow=2; export LS_COLORS
+
+    # enable starship theme prompt
+    starship init fish | source
+
+    # enable zoxide for better cd
+    zoxide init fish | source
+
 end
-
-# disable fish greeting
-set -U fish_greeting
-
-# fix sorting of files
-export LC_COLLATE="C"
-# change folder colors to be more readable
-set LS_COLORS ow=2; export LS_COLORS
-
-# enable zoxide
-eval "$(zoxide init fish)"
-
-# enable starship prompt
-starship init fish | source
-
